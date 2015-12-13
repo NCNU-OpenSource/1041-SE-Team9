b7c4a4f9ef5cd99011f3af1084478312cfd35275
ViperLiu <a0960330@gmail.com>
2015-12-13 23:47:06 +0800

    將登入頁面的CSS獨立出來
    登入頁面的CSS為:login.css

--- default.php
+++ default.php
@@ -3,26 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>開心農場</title>
-<style type="text/css">
-body {width:1300px; margin:10px auto;background-color:DarkOliveGreen; }
-#cont {
-width: 400px;
-margin: 20px auto;
-border-radius: 30px;
-background-color: #690;
-padding: 40px;
-color:ivory;
-font-size: 14pt;
-}
-#msg {
-font-weight: bold;
-color:red;
-}
-input {
-font-size: 14pt;
-font-family: Times New Roman;
-}
-</style>
+<link rel=stylesheet type="text/css" href="login.css">
 </head>
 <body>
 <div id="cont">
