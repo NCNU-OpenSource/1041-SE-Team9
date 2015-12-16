0cea15605a6e38ac0ddf645034cde4e0265e581c
ViperLiu <a0960330@gmail.com>
2015-12-16 09:47:07 +0800

    修正採收bug

--- get.php
+++ get.php
@@ -9,23 +9,24 @@ if (!$result) die("Query Fail!".mysqli_error($db_link));
 $row=mysqli_fetch_array($result);
 $plantid=$row['plantid'];//植物id
 
-//改變土地狀態
-$sql="update lands set status=0,plantid=0 where playerid='$user' and landid=$landid";
-$result=mysqli_query($db_link,$sql);
-if (!$result) die("Query Fail!".mysqli_error($db_link));
-
 //取得植物的數據
 $sql="select * from plants where plantid=$plantid";
 $result=mysqli_query($db_link,$sql);
 if (!$result) die("Query Fail!".mysqli_error($db_link));
 $row=mysqli_fetch_array($result);
-$getMoney=$row['sellvalue'];
-$getExp=$row['expget'];
+$getMoney=$row[2];
+$getExp=$row[4];
 
 //更新玩家狀態
-$sql="update user set money=money+$getMoney, exp=exp+$getExp where playerid='$user'";
+$sql="update `user` set money=money+$getMoney, exp=exp+$getExp, energy=energy-1 where id='$user'";
+echo $sql;
 $result=mysqli_query($db_link,$sql);
 if (!$result) die("Query Fail!".mysqli_error($db_link));
 $row=mysqli_fetch_array($result);
+
+//改變土地狀態
+$sql="update lands set status=0,plantid=0 where playerid='$user' and landid=$landid";
+$result=mysqli_query($db_link,$sql);
+if (!$result) die("Query Fail!".mysqli_error($db_link));
 
 ?>
