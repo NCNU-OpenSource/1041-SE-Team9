298b869ccfb1ab8609a1c80f8bb620a8ebab94a8
102213008tw <s102213008@mail1.ncnu.edu.tw>
2015-12-30 01:57:54 +0800

    main2k7CSS

--- main.php
+++ main.php
@@ -1,9 +1,35 @@
 ﻿<!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <meta http-equiv="refresh" content="30" />
-<link rel="stylesheet" type="text/css" href="main.css">
-<script src="functions.js"></script>
+  <title>開心農場</title>
+  <link rel="stylesheet" type="text/css" href="main.css">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+  <link rel="stylesheet" href="/resources/demos/style.css">
+<script src="functions.js">
+$(function() {
+    $( "#dialog" ).dialog({
+      autoOpen: false,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener" ).click(function() {
+      $( "#dialog" ).dialog( "open" );
+    });
+  });
+</script>
+<style>
+
+</style>
 </head>
 <body>
 <?php
@@ -11,7 +37,7 @@ include_once "islogin.php";
 include_once "connMysql.php";
 include_once "checklandstatus.php";
 ?>
-<div id="demo"><h2>開心農場</h2></div>
+<center><div id="demo"><h2><tr><td><img src="img/開心農場去背景.png"></td></tr></h2></div></center>
 
 <div>
 <?php 
@@ -33,17 +59,30 @@ for($i=0;$i<sizeof($available)-1;$i++){
     if($available[$i][2]>=0)
         $count++;//玩家可解鎖農地的數量
 }
+?>
+<div id="dialog" title="玩家資訊">
+  <p>
+ <?php 
 echo "<table>";
-echo "<tr><td>玩家名稱:$row[2]</td><tr>";
-echo "<tr><td>體力:$row[3]</td><tr>";
-echo "<tr><td>經驗值:$row[4]</td><tr>";
-echo "<tr><td>等級:$row[5]</td><tr>";
-echo "<tr><td>升級所需經驗:$row[6]</td><tr>";
+echo "玩家名稱:$row[2]";
+echo "體力:$row[3]";
+echo "經驗值:$row[4]";
+echo "等級:$row[5]";
+echo "升級所需經驗:$row[6]";
 echo "<tr><td id=\"playerMoney\">金錢:$row[7]</td><tr>";
-echo "</table>";
-echo "<a href=\"store.php\">商店</a> ";
-echo "<a href=\"bag.php\">背包</a><br>";
-echo"<table>";
+
+?>
+</p>
+</div>
+
+<button id="opener"><img src="img/農夫.jpg"></button>
+<div id="shop" >
+<?php
+echo "<h1><a href=\"store.php\"><img  id=shop  src=img/商店.jpg ></a></h1><br>";
+?>
+</div>
+<div id="ground" >
+<?php
 $index=1;
 for($i=0;$i<5;$i++){
     echo "<tr>";
@@ -55,22 +94,22 @@ for($i=0;$i<5;$i++){
             $disabled="disabled";
             if($count<$row[8])
                 $disabled="";
-            $status="尚未解鎖";
+            $status="<h2>尚未解鎖</h2>";
             echo "<td><button id=\"land{$index}\" title=\"{$status}\" onclick=\"unlock($index)\" {$disabled}>{$status}</button></td>";
         }
         else if($available[$index-1][2]==0){
             $disabled="";
-            $status="閒置";
+            $status="<h1>閒置</h1>";
         echo "<td><button id=\"land{$index}\" title=\"{$status}\" onclick=\"show($index)\" data=\"{$index}\" {$disabled}>{$status}</button></td>";
         }
         else if($available[$index-1][2]==1){
             $disabled="";
-            $status="生長中";
+            $status="<h3>生長中</h3>";
         echo "<td><button id=\"land{$index}\" title=\"{$status}\" value=\"{$available[$landid][3]}\" {$disabled}>{$status}</button></td>";
         }
         else if($available[$index-1][2]==2){
             $disabled="";
-            $status="待採收";
+            $status="<h4>待採收</h4>";
             echo "<td><button id=\"land{$index}\" title=\"{$status}\" onclick=\"get($index)\" {$disabled}>{$status}</button></td>";
         }
         $index++;
@@ -78,6 +117,7 @@ for($i=0;$i<5;$i++){
     echo "</tr>";
 }
 ?>
+</div>
     </table>
 </div>
 <div id="plantlist">
