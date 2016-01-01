b824c9176428f3f9f87722dda083be4498554a40
ViperLiu <a0960330@gmail.com>
2016-01-01 13:45:36 +0800

    新增升等功能
    升等功能

--- get.php
+++ get.php
@@ -3,6 +3,26 @@ include "connMysql.php";
 session_start();
 $user=$_SESSION["username"];
 $landid=$_GET["landid"];
+
+//取得玩家經驗值資訊
+$sql="select level,money,exp,exptonext from user where id='$user'";
+$result=mysqli_query($db_link,$sql);
+if (!$result) die("Query Fail!".mysqli_error($db_link));
+$row=mysqli_fetch_array($result);
+$level=$row['level'];
+$nextLevel=$level+2;
+$money=$row['money'];
+$currentExp=$row['exp'];
+$nextExp=$row['exptonext'];
+
+//取得等級資訊
+$sql="select * from levels where level=$nextLevel";
+$result=mysqli_query($db_link,$sql);
+if (!$result) die("Query Fail!".mysqli_error($db_link));
+$row=mysqli_fetch_array($result);
+$newNextExp=$row['exp'];
+
+
 $sql="select * from lands where playerid='$user' and landid=$landid";
 $result=mysqli_query($db_link,$sql);
 if (!$result) die("Query Fail!".mysqli_error($db_link));
@@ -18,11 +38,16 @@ $getMoney=$row[2];
 $getExp=$row[4];
 
 //更新玩家狀態
-$sql="update `user` set money=money+$getMoney, exp=exp+$getExp, energy=energy-1 where id='$user'";
-echo $sql;
+$money=$money+$getMoney;
+$currentExp=$currentExp+$getExp;
+if($currentExp>=$nextExp){
+    $sql="update user set money=$money,exp=$currentExp,level=level+1,exptonext=$newNextExp,energy=energy-1 where id='$user'";
+}
+else{
+    $sql="update user set money=$money,exp=$currentExp,energy=energy-1 where id='$user'";
+}
 $result=mysqli_query($db_link,$sql);
 if (!$result) die("Query Fail!".mysqli_error($db_link));
-$row=mysqli_fetch_array($result);
 
 //改變土地狀態
 $sql="update lands set status=0,plantid=0 where playerid='$user' and landid=$landid";
