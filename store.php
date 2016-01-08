d24b2f5cca0e977470ff01d8028c6c44490d895d
102213008tw <s102213008@mail1.ncnu.edu.tw>
2016-01-08 17:34:07 +0800

    改了一點STORE的位置和bag的箭頭位置

--- store.php
+++ store.php
@@ -21,9 +21,15 @@ echo "<tr><td>等級:$row[5]</td><tr>";
 echo "<tr><td>升級所需經驗:$row[6]</td><tr>";
 echo "<tr><td id=\"playerMoney\">金錢:$row[7]</td><tr>";
 echo "</table>";
-echo "<a href=\"main.php\">返回</a>";
 ?>
+
+<div id="backmain" >
+    <a href="main.php"><img  id="backmain" title="返回農場" src="img/返回用箭頭.png" width="50"></a>
+</div>
+
         <table>
+
+<div id="good">
 <?php
 $sql2="select * from bag where playerid='$user'";
 $result2=mysqli_query($db_link,$sql2);
@@ -38,14 +44,17 @@ if (!$result) die("Query Fail!".mysqli_error($db_link));
 $index=0;
 while($row[$index]=mysqli_fetch_array($result)){
     $foodid=$index+1;
-    echo"<tr><td>{$row[$index][1]}</td>
+    
+	echo"<tr><td>{$row[$index][1]}</td>
     <td>回復體力{$row[$index][3]}</td>
     <td>{$row[$index][2]}元</td>
     <td id=\"manyFood{$foodid}\">擁有{$row2[$index][2]}個</td>
     <td><button id=\"{$foodid}\" onclick=\"buy({$foodid})\">購買</button></td></tr>";
+	
     $index++;
 }
 ?>
+</div>
         </table>
         <p id="warn"></p>
     </body>
