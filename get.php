cf2c3c5456cfdd5a4744609d5d27f9c3ed308eac
ViperLiu <a0960330@gmail.com>
2016-01-08 15:05:15 +0800

    隨等級開放農地
    升等時會更新玩家可擁有的農地數量

--- get.php
+++ get.php
@@ -41,13 +41,15 @@ $getExp=$row[4];
 $money=$money+$getMoney;
 $currentExp=$currentExp+$getExp;
 if($currentExp>=$nextExp){
-    $sql="update user set money=$money,exp=$currentExp,level=level+1,exptonext=$newNextExp,energy=energy-1 where id='$user'";
+    $level=$level+1;
+    $sql="update user set money=$money,exp=$currentExp,level=level+1,exptonext=$newNextExp,energy=energy-1,landsavaliable=(select number from landnumber where level=$level)
+    where id='$user'";
 }
 else{
     $sql="update user set money=$money,exp=$currentExp,energy=energy-1 where id='$user'";
 }
 $result=mysqli_query($db_link,$sql);
-if (!$result) die("Query Fail!".mysqli_error($db_link));
+if (!$result) die("Query Fail!2".mysqli_error($db_link));
 
 //改變土地狀態
 $sql="update lands set status=0,plantid=0 where playerid='$user' and landid=$landid";
